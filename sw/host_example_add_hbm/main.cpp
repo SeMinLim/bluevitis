64203617bcea26bbf8168b52dd94038de42392e3
SeMinLim <seminl1@kookmin.ac.kr>
2026-03-31 13:17:25 +0900

    modified makefile

--- sw/host_example_add_hbm/main.cpp
+++ sw/host_example_add_hbm/main.cpp
@@ -35,18 +35,20 @@ int main(int argc, char** argv) {
 	}
 	
 	// Load XCLBIN
+	cout << "[Xilinx Alveo U50]" << endl;
+	fflush( stdout );
 	string xclbin_file = argv[1];
 	xrt::device device = xrt::device(DEVICE_ID);
 	xrt::uuid xclbin_uuid = device.load_xclbin(xclbin_file);
 	
 	// Create kernel object
-	cout << "Create Kernel" << endl;
-	fflush(stdout);
+	cout << "[STEP 1] Create Kernel" << endl;
+	fflush( stdout );	
 	auto krnl = xrt::kernel(device, xclbin_uuid, "kernel:{kernel_1}");
 
-	cout << "[Xilinx Alveo U50]" << endl;
-	cout << "Allocate Buffer in Global Memory" << endl;
-	fflush( stdout );
+	// Allocate buffer in global memory	
+	cout << "[STEP 2] Allocate Buffer in HBM" << endl;
+	fflush( stdout );	
 	auto boIn = xrt::bo(device, (size_t)DATA_SIZE, krnl.group_id(1));
 	auto boOut = xrt::bo(device, (size_t)DATA_SIZE, krnl.group_id(2)); 
 	
@@ -55,28 +57,34 @@ int main(int argc, char** argv) {
 	auto bo1_map = boOut.map<int*>();
 	fill(bo0_map, bo0_map + ((size_t)DATA_SIZE / 4), 0);
 	fill(bo1_map, bo1_map + ((size_t)DATA_SIZE / 4), 0);
+
+	// Fill global memory with data
 	bo0_map[0] = 1;
 	bo1_map[0] = 2;
 
 	// Synchronize buffer content with device side
-	cout << "Synchronize input buffer data to device global memory" << endl;
-	fflush(stdout);
+	cout << "[STEP 3] Synchronize input buffer data to device global memory" << endl;
+	fflush( stdout );
 	boIn.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 	boOut.sync(XCL_BO_SYNC_BO_TO_DEVICE);
 
-	cout << "Execution of the kernel" << endl;
-	fflush(stdout);
+	// Execute kernel
+	cout << "[STEP 4] Execution of the kernel" << endl;
+        fflush( stdout );	
 	auto run = krnl((size_t)DATA_SIZE, boIn, boOut);
 	run.wait();
 
 	// Get the output
-	cout << "Get the output data from the device" << endl;
-	fflush(stdout);
+	cout << "[STEP 5] Get the output data from the device" << endl;
+	fflush( stdout );
 	boOut.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
 
-	// Read cycle count
-	printf( "Result: %u\n", bo1_map[RESULTADDRESS] );
-	cout << "TEST PASSED" << endl;
+	// Verification
+	if ( bo1_map[RESULTADDRESS] == 3 ) {
+		cout << "TEST PASSED" << endl;
+	} else {
+		cout << "TEST FAILED" << endl;
+	}
 
 	return 0;
 }
